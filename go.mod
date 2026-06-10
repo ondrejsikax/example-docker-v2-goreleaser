ab0fc262a420099904567269e4fdece0fb6237df
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-10 17:53:38 +0200

    feat: Create example

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example-docker-v2-goreleaser
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+var Version = "dev"
+
+func main() {
+	fmt.Println("Hello from example-docker-v2-goreleaser " + Version + "!")
+}
